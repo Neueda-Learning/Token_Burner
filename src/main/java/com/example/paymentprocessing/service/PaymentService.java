6ec96c0a4d5362a095138e8f75f0030a7d8720f2
Richard Gao <dx1253602002@163.com>
2026-07-27 03:48:13 +0000

    Richard

--- src/main/java/com/example/paymentprocessing/service/PaymentService.java
+++ src/main/java/com/example/paymentprocessing/service/PaymentService.java
@@ -1,11 +1,12 @@
 package com.example.paymentprocessing.service;
 
 /**
- * TODO Member C:
+ * TODO Richard:
  * Define the service contract for payment use cases.
  * Add method signatures for creation, retrieval, history lookup, and status updates later.
  */
 public interface PaymentService {
     // TODO Add payment service method declarations.
+    
 }
 
